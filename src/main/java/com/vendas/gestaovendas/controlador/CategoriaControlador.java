10e79c00d5f75eebd9cc81ff0462f384d99ae721
vanderson nogueira <vanderson.01@hotmail.com>
2021-10-06 20:00:42 -0300

    Criar categoria

--- src/main/java/com/vendas/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/vendas/gestaovendas/controlador/CategoriaControlador.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,13 +31,25 @@ public class CategoriaControlador {
 
 	@GetMapping(path = "/{codigo}")
 	/**
-	 *  @param codigo da categoria 
-	 *  @return  um objeto optional de categoria caso a resposta seja ok.
-	 * */
+	 * @param codigo da categoria
+	 * @return um objeto optional de categoria caso a resposta seja ok.
+	 */
 	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable(name = "codigo") Long codigo) {
-		 
+
 		Optional<Categoria> optCategoria = service.buscarPorId(codigo);
 
 		return optCategoria.isPresent() ? ResponseEntity.ok(optCategoria) : ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	/**
+	 * @param categoria
+	 * @return um objeto optional de categoria, caso salvo com sucesso.
+	 */
+	public ResponseEntity<Categoria> buscarPorId(@RequestBody() Categoria categoria) {
+
+		Categoria categoriaSalva = service.salvar(categoria);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 }
--- src/main/java/com/vendas/gestaovendas/servico/CategoriaServico.java
+++ src/main/java/com/vendas/gestaovendas/servico/CategoriaServico.java
@@ -9,22 +9,24 @@ import org.springframework.stereotype.Service;
 import com.vendas.gestaovendas.entidades.Categoria;
 import com.vendas.gestaovendas.repositorio.CategoriaRepositorio;
 
-
 @Service
 public class CategoriaServico {
 
 	@Autowired
 	private CategoriaRepositorio repo;
-	
-	public List<Categoria> listarTodas(){
-		
+
+	public List<Categoria> listarTodas() {
+
 		return repo.findAll();
 	}
-	
-  public Optional<Categoria> buscarPorId(Long id ) {
-	  
-	  return repo.findById(id);
-  }
+
+	public Optional<Categoria> buscarPorId(Long id) {
+
+		return repo.findById(id);
+	}
+
+	public Categoria salvar(Categoria categoria) {
+
+		return repo.save(categoria);
+	}
 }
-
-
